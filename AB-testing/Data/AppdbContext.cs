143e3703652dea78cb011ed4543770989353f97c
Igor <igor19benderuk@gmail.com>
2024-02-22 13:41:18 +0200

    Add project files.

---
+++ AB-testing/Data/AppdbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AB_testing.Data
+{
+    public class AppdbContext : DbContext
+    {
+        public AppdbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ AB-testing/Program.cs
@@ -0,0 +1,41 @@
+
+using AB_testing.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AB_testing
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<AppdbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
